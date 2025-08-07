ce0215b9212111a786f4d45633de2e791ba695ee
Wanyong Shin <145497498+wanyongshinccf@users.noreply.github.com>
2025-08-07 13:46:29 -0400

    Update GenericfMRIVolumeProcessingPipelineBatch.ccf.sh

--- Scripts/GenericfMRIVolumeProcessingPipelineBatch.ccf.sh
+++ Scripts/GenericfMRIVolumeProcessingPipelineBatch.ccf.sh
@@ -158,7 +158,7 @@ SCRIPT_NAME=`basename "$0"`
 echo $SCRIPT_NAME
 
 if [ -n "${command_line_specified_task}" ]; then
-    TaskList=${command_line_specified_local}
+    TaskList=${command_line_specified_task}
 else
     TaskList=()
     TaskList+=(rfMRI_REST1_RL)
