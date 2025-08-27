2d42177519f05120091f062de9f855a92ab65c6a
Shin <shinw@ccf.org>
2025-08-27 13:14:28 -0400

    debugged

--- fMRIVolume/GenericfMRIVolumeProcessingPipeline.ccf.sh
+++ fMRIVolume/GenericfMRIVolumeProcessingPipeline.ccf.sh
@@ -924,8 +924,9 @@ fi
 if [[ $workstep = "all" ||  $workstep = "regout" ]]; then
     # SLOMOCO regress-out here
     echo "SLOMOCO: Regress out 13 vol-/sli-/voxel-regressors."
-    if [ -e $PhysioLogFile ]; then
-        PhysioStr="--phyregressor="$fMRIFolder"/Physio/RetroTS.PMU.slibase.1D "
+    Physio1DFile="$fMRIFolder"/Physio/RetroTS.PMU.slibase.1D
+    if [ -e $Physio1DFile ]; then
+        PhysioStr="--phyregressor=$Physio1DFile "
     else
         PhysioStr=" "
     fi
